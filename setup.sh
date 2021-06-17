bc8099da38155882b0f368ef916f171baf8a88bf
lchtao26 <lchtao26@icloud.com>
2021-06-17 15:02:48 +0800

    refactor(script): add process echos in `setup.sh`

--- setup.sh
+++ setup.sh
@@ -5,9 +5,12 @@ touch ~/.vim > /dev/null
 touch ~/.config/nvim > /dev/null
 mkdir -p ~/.bak/$now > /dev/null
 
-mv ~/.vim ~/.bak/$now/
-mv ~/.config/nvim ~/.bak/$now/
+echo '- Backup vim configuration'
+mv -v ~/.vim ~/.bak/$now/
+mv -v ~/.config/nvim ~/.bak/$now/
 
+echo '\n'
+echo '- Create symbolic link'
 # Create nvim init file
 ln -sfv `pwd`/dotVim/vimrc `pwd`/dotVim/init.vim
 # Link to neovim
@@ -18,3 +21,7 @@ ln -sv `pwd`/dotVim ~/.vim
 # Fetch bundles
 git submodule init
 git submodule update
+
+echo '\n'
+echo '- Setup complete!'
+
