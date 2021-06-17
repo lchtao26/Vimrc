8f4ff63bef943724a90325cc4d0e86c79cd9f308
lchtao26 <lchtao26@icloud.com>
2021-06-17 14:39:04 +0800

    refactor(script): update `setup.sh`, for better initializing in other computer

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
+# Prepared for linking
+now=`date '+%F_%H%M%S'`
+
+touch ~/.vim > /dev/null
+touch ~/.config/nvim > /dev/null
+mkdir -p ~/.bak/$now > /dev/null
+
+mv ~/.vim ~/.bak/$now/
+mv ~/.config/nvim ~/.bak/$now/
+
+# Create nvim init file
+ln -sfv `pwd`/dotVim/vimrc `pwd`/dotVim/init.vim
+# Link to neovim
+ln -sv `pwd`/dotVim ~/.config/nvim
+# Link to vim
+ln -sv `pwd`/dotVim ~/.vim
+
+# Fetch bundles
 git submodule init
 git submodule update
-
-ln -s `pwd`/dotVim/vimrc `pwd`/dotVim/init.vim          # Create nvim init file
-ln -s `pwd`/dotVim ~/.vim          # Link to vim
-ln -s `pwd`/dotVim ~/.config/nvim  # Link to neovim
