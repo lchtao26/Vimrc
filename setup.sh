a77d941c176ca25512683752053c11505789b73e
lchtao26 <lchtao26@icloud.com>
2021-07-01 10:15:28 +0800

    chore(setup.sh): remove neovim setup, only setup for vim

--- setup.sh
+++ setup.sh
@@ -2,19 +2,13 @@
 now=`date '+%F_%H%M%S'`
 
 touch ~/.vim > /dev/null
-touch ~/.config/nvim > /dev/null
 mkdir -p ~/.bak/$now > /dev/null
 
 echo '- Backup vim configuration'
 mv -v ~/.vim ~/.bak/$now/
-mv -v ~/.config/nvim ~/.bak/$now/
 
 echo '\n'
 echo '- Create symbolic link'
-# Create nvim init file
-ln -sfv `pwd`/dotVim/vimrc `pwd`/dotVim/init.vim
-# Link to neovim
-ln -sv `pwd`/dotVim ~/.config/nvim
 # Link to vim
 ln -sv `pwd`/dotVim ~/.vim
 
